6aebf6a3046467c344fafa5cb0793d452bbca04a
tongjian <jeff.tong@3nod.com.cn>
2022-07-19 14:33:36 +0800

    update

--- src/view/menu/index.vue
+++ src/view/menu/index.vue
@@ -1,34 +1,43 @@
 <template>
-  <div  >
-    <router-view></router-view>
-    <div class="menu-wrapper" v-show="$route.meta.index===0">
-      <van-grid square :column-num="3">
-        <van-grid-item icon="fire" text="物料预警" to="/menu/material"/>
-      </van-grid>
+    <div>
+        <router-view></router-view>
+        <div class="menu-wrapper" v-show="$route.meta.index===0">
+            <van-grid square :column-num="3">
+                <van-grid-item icon="fire" text="物料预警" to="/menu/material"/>
+                <van-grid-item icon="bill" text="报工" color="#1989fa" :url="reportUrl"/>
+            </van-grid>
+        </div>
     </div>
-  </div>
-
 </template>
 
 <script>
-export default {
-  name: "menu"
-}
+    export default {
+        name: "menu",
+        data() {
+            return {
+                reportUrl: process.env.VUE_APP_REPORT_URL
+            }
+        },
+        created() {
+            // window.location.replace( location.href );
+            
+        }
+    }
 </script>
 
 <style scoped>
-.menu-wrapper {
-  width: 100%;
-  height: 100%;
-  background-color: #f5f5f5
-}
+    .menu-wrapper {
+        width: 100%;
+        height: 100%;
+        background-color: #f5f5f5
+    }
 
-::v-deep(.van-grid-item__text) {
-  font-size: 16px;
-  color: #323233;
-}
+    ::v-deep(.van-grid-item__text) {
+        font-size: 16px;
+        color: #323233;
+    }
 
-::v-deep(.van-grid-item__icon) {
-  color: red !important;
-}
+    ::v-deep(.van-grid-item__icon) {
+        color: red !important;
+    }
 </style>
